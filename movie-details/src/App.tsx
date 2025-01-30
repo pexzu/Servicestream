cfca34f0fa683a69273e3fd0a799b1071b064996
VJ <vichuvj92@gmail.com>
2025-01-30 16:34:18 +1100

    Basic intial setup
    Typescript, styled component and version issues resolved

---
+++ movie-details/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Table } from './Component/Table/Table';
+
+function App() {
+  return ( 
+  <>
+    <Table/>
+  </>
+  );
+}
+
+export default App;
---
+++ movie-details/src/Component/Table/Table.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as E from './TableElement'
+
+export const Table = () =>{
+    return(
+        <E.Button> Test</E.Button>
+    )
+}
